e9b5f4e0b94a64bbc0ce919c949f47bc39e845a7
Marcel <malbrecht0792@gmail.com>
2018-02-21 13:58:24 -0500

    Finished the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,47 @@
-console.log('hi');
+var player1Name = 'Player 1';
+var player2Name = 'Player 2';
+var player1wins = 0;
+var player2wins = 0;
+var weapons = ['rock', 'paper', 'scissors'];
+
+while(player1wins < 3 && player2wins <3) {
+  var weaponOfChoice1 =
+      weapons[Math.floor(Math.random() * weapons.length) % 3];
+  var weaponOfChoice2 =
+      weapons[Math.floor(Math.random() * weapons.length) % 3];
+  console.log('Player 1: ' + weaponOfChoice1);
+  console.log('Player 2: ' + weaponOfChoice2);
+  if(weaponOfChoice1 === 'rock' && weaponOfChoice2 === 'scissors') {
+    player1wins++;
+    console.log('Player 1 wins');
+  } else if(weaponOfChoice1 === 'paper' && weaponOfChoice2 === 'rock') {
+    player1wins++;
+    console.log('Player 1 wins');
+  } else if(weaponOfChoice1 === 'scissors' && weaponOfChoice2 === 'paper') {
+    player1wins++;
+    console.log('Player 1 wins');
+  } else if(weaponOfChoice1 === 'rock' && weaponOfChoice2 === 'paper') {
+    player2wins++;
+    console.log('Player 2 wins');
+  } else if(weaponOfChoice1 === 'paper' && weaponOfChoice2 === 'scissors') {
+    player2wins++;
+    console.log('Player 2 wins');
+  } else if(weaponOfChoice1 === 'scissors' && weaponOfChoice2 === 'rock') {
+    player2wins++;
+    console.log('Player 2 wins');
+  } else {
+    console.log('It\'s a tie');
+  }
+
+  console.log('Number of wins for Player 1: ' + player1wins);
+  console.log('Number of wins for Player 2: ' + player2wins);
+  console.log(' ');
+}
+
+if(player1wins === 3) {
+  console.log('Player 1 wins the game!');
+} else {
+  console.log('Player 2 wins the game!');
+}
+
+
